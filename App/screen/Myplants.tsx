afc4d7829f0bca53a1851cd8c9f991203c9f3b27
arichardi <a.richardi@gmail.com>
2021-08-02 18:16:03 -0300

    Icons SVG and Fonts Workig

--- App/screen/Myplants.tsx
+++ App/screen/Myplants.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Container, Title, Underline } from './MyPlantsStyles';
+import BrotoIcon from '../Assets/BrotoIcon';
 
 export default function MyPlants(){
     return(
         <Container>
             <Underline />
             <Title>Minhas Plantas</Title>
+            <BrotoIcon />
         </Container>
     );
 }
